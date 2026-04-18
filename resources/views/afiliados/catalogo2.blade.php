ec746ab2d358f8fb88e43a114f863e7ee749b706
sampaiofree <sampaio.free@gmail.com>
2026-04-18 08:49:40 -0300

    Markdow

--- resources/views/afiliados/catalogo2.blade.php
+++ resources/views/afiliados/catalogo2.blade.php
@@ -9,14 +9,10 @@
 
 - ID: {!! $item['id'] !!}
 - Descrição: {!! $item['description'] !== '' ? $item['description'] : 'Não informada' !!}
-- Disponibilidade: {!! $item['availability'] !!}
-- Condição: {!! $item['condition'] !!}
 - Preço: {!! $item['price'] !!}
 - Link da página: {!! $item['link'] !== '' ? $item['link'] : 'Não disponível' !!}
 - Link do checkout: {!! $item['checkout_link'] !== '' ? $item['checkout_link'] : 'Não disponível' !!}
 - Carga horária: {!! $item['workload'] !== '' ? $item['workload'] : 'Não informada' !!}
 - Professor: {!! $item['teacher_name'] !== '' ? $item['teacher_name'] : 'Não informado' !!}
-- Imagem: {!! $item['image_link'] !== '' ? $item['image_link'] : 'Sem imagem' !!}
-- Marca: {!! $item['brand'] !!}
 
 @endforeach
--- tests/Feature/AfiliadoCatalogoLinkTest.php
+++ tests/Feature/AfiliadoCatalogoLinkTest.php
@@ -46,4 +46,50 @@ class AfiliadoCatalogoLinkTest extends TestCase
         );
         $response->assertDontSee('?ref=', false);
     }
+
+    public function test_catalogo_markdown_hides_internal_fields_and_keeps_expected_course_data(): void
+    {
+        $user = User::factory()->create([
+            'dominio' => 'afiliado-markdown.portalje.test',
+        ]);
+
+        $curso = Curso::create([
+            'ordem' => 1,
+            'url' => 'curso-atendente-farmacia',
+            'titulo' => 'Curso Atendente de Farmácia',
+            'headline' => 'Curso teste para validar o catálogo markdown.',
+            'descricao_curta' => 'Aprenda rotinas práticas para trabalhar em farmácias.',
+            'gratuito' => false,
+            'publicado' => true,
+            'permitir_afiliacao' => true,
+            'mostrar_na_pagina' => true,
+            'preco_cheio_completo' => 'R$297',
+            'link_checkout_completo' => 'https://checkout.portalje.test/curso-atendente-farmacia',
+            'horas_completo' => '160',
+            'professor_nome' => 'Maria Souza',
+        ]);
+
+        Codigo_ref::create([
+            'user_id' => $user->id,
+            'curso_id' => $curso->id,
+            'codigo_ref' => 'REFMD123',
+            'mostrar_curso' => true,
+        ]);
+
+        $response = $this->get("http://{$user->dominio}/afiliado/catalogo2");
+
+        $response->assertOk();
+        $response->assertHeader('Content-Type', 'text/markdown; charset=UTF-8');
+        $response->assertSee('Curso Atendente de Farmácia');
+        $response->assertSee('Aprenda rotinas práticas para trabalhar em farmácias.');
+        $response->assertSee("- Link da página: http://{$user->dominio}/{$curso->url}", false);
+        $response->assertSee('- Link do checkout: https://checkout.portalje.test/curso-atendente-farmacia', false);
+        $response->assertSee('- Carga horária: 160 horas', false);
+        $response->assertSee('- Professor: Maria Souza', false);
+        $response->assertSee('- Preço: 148.50 BRL', false);
+        $response->assertDontSee('Disponibilidade:', false);
+        $response->assertDontSee('Condição:', false);
+        $response->assertDontSee('Imagem:', false);
+        $response->assertDontSee('Marca:', false);
+    }
 }
